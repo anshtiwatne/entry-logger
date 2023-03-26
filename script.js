6707ef3380b5737bedc015f69b708638db952072
Ansh Tiwatne <ansh.tiwatne@gmail.com>
2023-03-26 15:46:43 +0530

    setup firebase

---
+++ script.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWM7GeKuvOC6_vTVJTA-eSfTDB1jt8LbQ",
+  authDomain: "entry-logger.firebaseapp.com",
+  projectId: "entry-logger",
+  storageBucket: "entry-logger.appspot.com",
+  messagingSenderId: "541559372059",
+  appId: "1:541559372059:web:c2345f2d13f1cd92d9fbbe"
+};
+
+const app = initializeApp(firebaseConfig);
