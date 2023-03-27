b300525a0fa1f7e2b9f2a917615e16d21304e899
Ansh <ansh.tiwatne@gmail.com>
2023-03-27 17:16:22 +0530

    added firestore

--- public/app.js
+++ public/app.js
@@ -8,7 +8,7 @@ function googleSignIn() {
     const provider = new firebase.auth.GoogleAuthProvider()
     firebase.auth().signInWithPopup(provider)
         .then(result => {
-            const user = result.user
+            user = result.user
         })
         .catch(console.log)
 }
@@ -16,35 +16,54 @@ function googleSignIn() {
 firebase.auth().onAuthStateChanged(user => {
 
     if (user) {
-        const datetime = new Date(); // to use
-        if (document.getElementById("entry").checked == true) {
-            innerHTML = `${user.displayName}'s entry logged`
-        }
-        else if (document.getElementById("exit").checked == true) {
-            innerHTML = `${user.displayName}'s exit logged`
-        }
-        document.getElementById("log").innerHTML = innerHTML
+        document.getElementById("log").innerHTML = `${user.displayName} logged in`
     }
     else {
         // not signed in
     }
 })
 
+let position
+
 function confirmLocation() {
-    navigator.geolocation.getCurrentPosition(showPosition)
+    navigator.geolocation.getCurrentPosition(checkPosition)
 }
 
-function showPosition(position) {
-    const usr_location = position.coords
+function checkPosition(pos) {
+    const usr_location = pos.coords
     const school_loc = {latitude: 18.5579709, longitude: 73.7830746}
+
     if (usr_location.latitude.toFixed(1) == school_loc.latitude.toFixed(1) && usr_location.longitude.toFixed(1) == school_loc.longitude.toFixed(1)) {
-        console.log("user location confirmed")
-        document.getElementById("locationText").innerHTML = "Location confirmed"
-        return true
+        document.getElementById("log").innerHTML = "Location confirmed"
+        position = true
     }
     else {
         console.log(usr_location.latitude.toFixed(0), school_loc.latitude.toFixed(0), usr_location.longitude.toFixed(0), school_loc.longitude.toFixed(0))
-        document.getElementById("locationText").innerHTML = "Location not accurate"
-        return false
+        document.getElementById("log").innerHTML = "Location not accurate"
+        position = false
+    }
+}
+
+const db = firebase.firestore()
+
+function logEntry() {
+    const datetime = new Date()
+    console.log(position)
+    if (user) {
+        if (position) {
+            if (document.getElementById("entry").checked == true) {
+                logType = "entry"
+            }
+            else if (document.getElementById("exit").checked == true) {
+                logType = "exit"
+            }
+            attendance = db.collection("attendance")
+            attendance.add({
+                name: user.displayName,
+                status: logType,
+                time: datetime.toISOString()
+            })
+            document.getElementById("log").innerHTML = `${user.displayName}'s ${logType} logged`
+        }
     }
 }
