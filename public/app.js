9c11ec65cfaae5062548e8828160e78aafe7a036
Ansh <ansh.tiwatne@gmail.com>
2023-03-27 14:37:35 +0530

    added location confirmation

--- public/app.js
+++ public/app.js
@@ -14,15 +14,14 @@ function googleSignIn() {
 }
 
 firebase.auth().onAuthStateChanged(user => {
+
     if (user) {
-        const datetime = new Date();
-        const time = datetime.getTime();
-        console.log("hellllllloooooooooooo")
+        const datetime = new Date(); // to use
         if (document.getElementById("entry").checked == true) {
-            innerHTML = `${user.displayName}'s entry logged at ${time}`
+            innerHTML = `${user.displayName}'s entry logged`
         }
         else if (document.getElementById("exit").checked == true) {
-            innerHTML = `${user.displayName}'s exit logged at ${time}`
+            innerHTML = `${user.displayName}'s exit logged`
         }
         document.getElementById("log").innerHTML = innerHTML
     }
@@ -31,6 +30,21 @@ firebase.auth().onAuthStateChanged(user => {
     }
 })
 
-function checkLocation() {
-    // hello
+function confirmLocation() {
+    navigator.geolocation.getCurrentPosition(showPosition)
+}
+
+function showPosition(position) {
+    const usr_location = position.coords
+    const school_loc = {latitude: 18.5579709, longitude: 73.7830746}
+    if (usr_location.latitude.toFixed(1) == school_loc.latitude.toFixed(1) && usr_location.longitude.toFixed(1) == school_loc.longitude.toFixed(1)) {
+        console.log("user location confirmed")
+        document.getElementById("locationText").innerHTML = "Location confirmed"
+        return true
+    }
+    else {
+        console.log(usr_location.latitude.toFixed(0), school_loc.latitude.toFixed(0), usr_location.longitude.toFixed(0), school_loc.longitude.toFixed(0))
+        document.getElementById("locationText").innerHTML = "Location not accurate"
+        return false
+    }
 }
