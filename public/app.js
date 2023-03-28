5e9255da8e0dff03bc4787dc4cb7beefe09b08dd
Ansh <ansh.tiwatne@gmail.com>
2023-03-28 16:42:35 +0530

    added diabled button styling

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", event => {
     const app = firebase.app()
 })
 
-firebase.auth().signOut()
+// firebase.auth().signOut()
 
 function googleSignIn() {
     const provider = new firebase.auth.GoogleAuthProvider()
@@ -16,7 +16,8 @@ function googleSignIn() {
 firebase.auth().onAuthStateChanged(user => {
 
     if (user) {
-        document.getElementById("log").innerHTML = `${user.displayName} logged in`
+        document.getElementById("googleSignInText").innerHTML = `${user.displayName} logged in`
+        document.getElementById("googleSignIn").disabled = true
     }
     else {
         // not signed in
@@ -34,12 +35,13 @@ function checkPosition(pos) {
     const school_loc = {latitude: 18.5579709, longitude: 73.7830746}
 
     if (usr_location.latitude.toFixed(1) == school_loc.latitude.toFixed(1) && usr_location.longitude.toFixed(1) == school_loc.longitude.toFixed(1)) {
-        document.getElementById("log").innerHTML = "Location confirmed"
+        document.getElementById("confirmLocation").innerHTML = "Location confirmed"
+        document.getElementById("confirmLocation").disabled = true
         position = true
     }
     else {
         console.log(usr_location.latitude.toFixed(0), school_loc.latitude.toFixed(0), usr_location.longitude.toFixed(0), school_loc.longitude.toFixed(0))
-        document.getElementById("log").innerHTML = "Location not accurate"
+        document.getElementById("confirmLocation").innerHTML = "Location not accurate"
         position = false
     }
 }
@@ -63,7 +65,10 @@ function logEntry() {
                 status: logType,
                 time: datetime.toISOString()
             })
-            document.getElementById("log").innerHTML = `${user.displayName}'s ${logType} logged`
+            document.getElementById("logEntry").innerHTML = `${logType} logged`.charAt(0).toUpperCase() + `${logType} logged`.slice(1)
         }
+    }
+    else {
+        document.getElementById("logEntry").innerHTML = "Retry"
     }
 }
